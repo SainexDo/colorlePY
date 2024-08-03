082e4de8fe4f208fbea4d35665c10ae414f3d468
SainexDo <ventizgene@gmail.com>
2024-08-03 06:22:40 -0600

    El jugador ya puede generar una secuencia de colores.

--- app/game.py
+++ app/game.py
@@ -6,12 +6,12 @@ import random
 class Game:
     #Juego, dividido en sus respectivas clases y métodos.
     def __init__(self, rojo, azul, verde, amarillo, reset):
-        #Declaramos variables privadas.
         self.rojo = rojo
         self.azul = azul
         self.verde = verde
         self.amarillo = amarillo
         self.reset = reset
+        #Declaramos variables privadas.
         self.secuencia = []
         self.intentoDeAdivinarSecuencia = []
     
@@ -26,9 +26,32 @@ class Game:
                 elif respuesta == 'crear':
                     self.creaJugador()
                 break
+    
+        
+                
+                
                 
     def creaJugador(self):
-        print("ga")
+        posiblesOpciones = (self.rojo, self.azul, self.amarillo, self.verde)
+        while True:
+            opcion = input("Elige los colores de tu secuencia (r/b/y/g)")
+            if len(self.secuencia) <= 4:
+                if opcion == "r":
+                    self.secuencia.append(posiblesOpciones[0])
+                    print("|", "".join(self.secuencia) + self.reset, "" + "|") 
+                elif opcion == "b":
+                    self.secuencia.append(posiblesOpciones[1])
+                    print("|", "".join(self.secuencia) + self.reset, "" + "|")
+                elif opcion == "y":
+                    self.secuencia.append(posiblesOpciones[2])
+                    print("|", "".join(self.secuencia) + self.reset, "" + "|")
+                elif opcion == "g":
+                    self.secuencia.append(posiblesOpciones[3])
+                    print("|", "".join(self.secuencia) + self.reset, "|")
+                if len(self.secuencia) == 4:
+                    break
+        
+        
 
     def creaComputadora(self):
         while True:
@@ -37,11 +60,6 @@ class Game:
             if len(self.secuencia) == 4:
                 print(''.join(self.secuencia) + self.reset)
                 break
-        
-        intentos = []
-        # while True:
-        #     print("Manejo")
-        #     break
 
 def main():
     Juego = Game(azul=(Fore.BLUE + " O "), rojo=(Fore.RED + " O "), amarillo=(Fore.YELLOW + " O "), verde=(Fore.GREEN + " O "), reset=Fore.RESET)
